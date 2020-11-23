5fd61681d66a98c927ba4b6052542e9f94d0e37a
WASYL94 <adminwroc@gmail.com>
2020-11-23 23:01:58 +0100

    first commit

---
+++ controllers/courses.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const router = express.Router();
+const CourseModel = mongoose.model("Course")
+
+router.get("/add", (req, res)=>{
+    res.render("add-course")
+})
+
+//setting
+// var course = new CourseModel();
+// course.courseName = "NodeJS";
+// course.courseId = "2";
+// course.save();
+
+router.post("/add", (req, res)=>{
+
+    var course = new CourseModel();
+    course.courseName = req.body;
+    course.courseDuration = req.body.courseDuration;
+    course.courseFee = req.body.courseFee;
+    course.courseId = Math.ceil(Math.random() * 100000000);
+
+    course.save((err, doc)=>{
+        if(!err)
+            {
+                //res.redirect("/course/list")
+                res.json({ message : "succesfull", status : "OK"});
+            }
+        else
+            {
+                res.send("Error Occured");
+            }
+    });
+
+
+    res.render("add-course")
+})
+
+router.get("/list", (req, res)=>{
+
+    //Getting
+    CourseModel.find((err, docs)=>{
+        if(!err)
+            {
+                console.log(docs);
+                res.render("list", { data : docs });
+            }
+        else
+            {
+                res.send("Error");
+            }  
+    });
+});
+
+module.exports = router;
